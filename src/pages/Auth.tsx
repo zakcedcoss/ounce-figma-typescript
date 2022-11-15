00dca28f11260bc24bbe41aacb280e7947911b34
zakcedcoss <zeeshanahmadkhan@cedcommerce.com>
2022-11-15 17:08:56 +0530

    page footer added

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,9 @@
 import {
+  BodyLayout,
   Card,
   FlexLayout,
   FormElement,
+  PageFooter,
   TextField,
   TextStyles,
   Toast,
@@ -117,13 +119,11 @@ function Auth() {
         !inputs.password2
       ) {
         setErrors({ active: true, message: "Fill all the fields" });
-        setInputs(initialInputs);
         return;
       }
     } else if (auth === "login") {
       if (!inputs?.email || !inputs?.password) {
         setErrors({ active: true, message: "Fill all the fields" });
-        setInputs(initialInputs);
         return;
       }
     }
@@ -132,155 +132,169 @@ function Auth() {
   };
 
   return (
-    <Card>
-      <FlexLayout valign="center" direction="vertical">
-        <TextStyles type="Heading" alignment="center">
-          Welcome
-        </TextStyles>
-        <div
-          style={{
-            width: 450,
-            marginInline: "auto",
-          }}
-        >
-          <Card
-            cardType="Shadowed"
-            title={auth === "login" ? "Login" : "Register New User"}
-            primaryAction={{
-              type: "Primary",
-              content: auth === "login" ? "Login" : "Register",
-              onClick: handleSubmit,
+    <BodyLayout>
+      <Card>
+        <FlexLayout valign="center" direction="vertical">
+          <TextStyles type="Heading" alignment="center">
+            Welcome
+          </TextStyles>
+          <div
+            style={{
+              width: 450,
+              marginInline: "auto",
             }}
           >
-            <FormElement>
-              <FlexLayout
-                desktopWidth="100"
-                mobileWidth="100"
-                spacing="loose"
-                tabWidth="100"
-                direction="vertical"
-              >
-                {auth !== "login" && (
+            <Card
+              cardType="Shadowed"
+              title={auth === "login" ? "Login" : "Register New User"}
+              primaryAction={{
+                type: "Primary",
+                content: auth === "login" ? "Login" : "Register",
+                onClick: handleSubmit,
+              }}
+            >
+              <FormElement>
+                <FlexLayout
+                  desktopWidth="100"
+                  mobileWidth="100"
+                  spacing="loose"
+                  tabWidth="100"
+                  direction="vertical"
+                >
+                  {auth !== "login" && (
+                    <TextField
+                      error={
+                        errorMessages?.fname ? errorMessages?.fname[0] : false
+                      }
+                      showHelp={
+                        errorMessages?.fname ? errorMessages?.fname[1] : ""
+                      }
+                      autocomplete="off"
+                      name="First Name"
+                      onChange={(e) => handleChange(e, "fname")}
+                      placeHolder="Enter First Name"
+                      type="text"
+                      value={inputs.fname}
+                    />
+                  )}
+                  {auth !== "login" && (
+                    <TextField
+                      error={
+                        errorMessages?.lname ? errorMessages?.lname[0] : false
+                      }
+                      showHelp={
+                        errorMessages?.lname ? errorMessages?.lname[1] : ""
+                      }
+                      autocomplete="off"
+                      name="Last Name"
+                      onChange={(e) => handleChange(e, "lname")}
+                      placeHolder="Enter Last Name"
+                      type="text"
+                      value={inputs.lname}
+                    />
+                  )}
+                  {auth !== "login" && (
+                    <TextField
+                      error={
+                        errorMessages?.username
+                          ? errorMessages?.username[0]
+                          : false
+                      }
+                      showHelp={
+                        errorMessages?.username
+                          ? errorMessages?.username[1]
+                          : ""
+                      }
+                      autocomplete="off"
+                      name="Username"
+                      onChange={(e) => handleChange(e, "username")}
+                      placeHolder="Enter Username"
+                      type="text"
+                      value={inputs.username}
+                    />
+                  )}
                   <TextField
                     error={
-                      errorMessages?.fname ? errorMessages?.fname[0] : false
+                      errorMessages?.email ? errorMessages?.email[0] : false
                     }
                     showHelp={
-                      errorMessages?.fname ? errorMessages?.fname[1] : ""
+                      errorMessages?.email ? errorMessages?.email[1] : ""
                     }
                     autocomplete="off"
-                    name="First Name"
-                    onChange={(e) => handleChange(e, "fname")}
-                    placeHolder="Enter First Name"
-                    type="text"
-                    value={inputs.fname}
+                    name="Email"
+                    onChange={(e) => handleChange(e, "email")}
+                    placeHolder="Enter Email"
+                    type="email"
+                    value={inputs.email}
                   />
-                )}
-                {auth !== "login" && (
                   <TextField
                     error={
-                      errorMessages?.lname ? errorMessages?.lname[0] : false
-                    }
-                    showHelp={
-                      errorMessages?.lname ? errorMessages?.lname[1] : ""
-                    }
-                    autocomplete="off"
-                    name="Last Name"
-                    onChange={(e) => handleChange(e, "lname")}
-                    placeHolder="Enter Last Name"
-                    type="text"
-                    value={inputs.lname}
-                  />
-                )}
-                {auth !== "login" && (
-                  <TextField
-                    error={
-                      errorMessages?.username
-                        ? errorMessages?.username[0]
+                      errorMessages?.password
+                        ? errorMessages?.password[0]
                         : false
                     }
                     showHelp={
-                      errorMessages?.username ? errorMessages?.username[1] : ""
+                      errorMessages?.password ? errorMessages?.password[1] : ""
                     }
                     autocomplete="off"
-                    name="Username"
-                    onChange={(e) => handleChange(e, "username")}
-                    placeHolder="Enter Username"
-                    type="text"
-                    value={inputs.username}
-                  />
-                )}
-                <TextField
-                  error={errorMessages?.email ? errorMessages?.email[0] : false}
-                  showHelp={errorMessages?.email ? errorMessages?.email[1] : ""}
-                  autocomplete="off"
-                  name="Email"
-                  onChange={(e) => handleChange(e, "email")}
-                  placeHolder="Enter Email"
-                  type="email"
-                  value={inputs.email}
-                />
-                <TextField
-                  error={
-                    errorMessages?.password ? errorMessages?.password[0] : false
-                  }
-                  showHelp={
-                    errorMessages?.password ? errorMessages?.password[1] : ""
-                  }
-                  autocomplete="off"
-                  name="Password"
-                  onChange={(e) => handleChange(e, "password")}
-                  placeHolder="Enter Password"
-                  type="password"
-                  value={inputs.password}
-                />
-                {auth !== "login" && (
-                  <TextField
-                    error={
-                      errorMessages?.password2
-                        ? errorMessages?.password2[0]
-                        : false
-                    }
-                    showHelp={
-                      errorMessages?.password2
-                        ? errorMessages?.password2[1]
-                        : ""
-                    }
-                    autocomplete="off"
-                    name="Re-Enter Password"
-                    onChange={(e) => handleChange(e, "password2")}
-                    placeHolder="Enter Password Again"
+                    name="Password"
+                    onChange={(e) => handleChange(e, "password")}
+                    placeHolder="Enter Password"
                     type="password"
-                    value={inputs.password2}
+                    value={inputs.password}
                   />
-                )}
-              </FlexLayout>
-            </FormElement>
-          </Card>
-          <Card>
-            {auth === "login" ? (
-              <TextStyles>
-                Not a member ? <Link to="/auth/signup">Sign Up</Link> instead
-              </TextStyles>
-            ) : (
-              <TextStyles>
-                Already a memeber ? <Link to="/auth/login">Login</Link>
-              </TextStyles>
-            )}
-          </Card>
-        </div>
-      </FlexLayout>
-      {errors?.active && (
-        <ToastWrapper>
-          <Toast
-            message={errors.message}
-            type="error"
-            onDismiss={() => setErrors({ active: false, message: "" })}
-            timeout={3000}
-          />
-        </ToastWrapper>
-      )}
-    </Card>
+                  {auth !== "login" && (
+                    <TextField
+                      error={
+                        errorMessages?.password2
+                          ? errorMessages?.password2[0]
+                          : false
+                      }
+                      showHelp={
+                        errorMessages?.password2
+                          ? errorMessages?.password2[1]
+                          : ""
+                      }
+                      autocomplete="off"
+                      name="Re-Enter Password"
+                      onChange={(e) => handleChange(e, "password2")}
+                      placeHolder="Enter Password Again"
+                      type="password"
+                      value={inputs.password2}
+                    />
+                  )}
+                </FlexLayout>
+              </FormElement>
+            </Card>
+            <Card>
+              {auth === "login" ? (
+                <TextStyles>
+                  Not a member ? <Link to="/auth/signup">Sign Up</Link> instead
+                </TextStyles>
+              ) : (
+                <TextStyles>
+                  Already a memeber ? <Link to="/auth/login">Login</Link>
+                </TextStyles>
+              )}
+            </Card>
+          </div>
+        </FlexLayout>
+        {errors?.active && (
+          <ToastWrapper>
+            <Toast
+              message={errors.message}
+              type="error"
+              onDismiss={() => setErrors({ active: false, message: "" })}
+              timeout={3000}
+            />
+          </ToastWrapper>
+        )}
+      </Card>
+      <PageFooter>
+        <TextStyles>Cedcommerce @ 2022</TextStyles>
+        <TextStyles>Coded by Zeeshan A. Khan</TextStyles>
+      </PageFooter>
+    </BodyLayout>
   );
 }
 
